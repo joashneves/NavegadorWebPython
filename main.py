0bf4dafc1c5815c02a9da0c8c1aad6f5e2bab0c0
Joashneves@gmail.com <joashneves@gmail.com>
2024-03-10 18:53:06 -0300

    coloando ou tentando arrumar style

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ class MainWindow(QMainWindow):
 
         #Adicionando Menu
         menuBox = QComboBox()
-        menuBox.addItems(['','Opções', 'Histórico'])
+        menuBox.addItems(['...','Opções', 'Histórico'])
         # Definir ícone apenas para o primeiro item
         menuBox.setItemIcon(0, QIcon('img/settings.svg'))  # Ícone para 'Opções'
         menuBox.currentIndexChanged.connect(self.opcao_selecionada)
